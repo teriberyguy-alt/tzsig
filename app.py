06804555a156091bb088c9433d189e4576e6317e
teriberyguy-alt <teriberyguy@gmail.com>
2026-02-03 06:30:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "TZ SERVER ONLINE"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
